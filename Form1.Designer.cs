90f376b21d7f9400024220e44791537b8d57ceca
S3lv3r <131556687+S3lv3r@users.noreply.github.com>
2025-03-29 20:14:31 -0600

    Posicion del label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,30 +29,44 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(183, 163);
+            button1.Location = new Point(138, 130);
             button1.Name = "button1";
             button1.Size = new Size(479, 220);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(300, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Prueba de Cambio";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
     }
 }
